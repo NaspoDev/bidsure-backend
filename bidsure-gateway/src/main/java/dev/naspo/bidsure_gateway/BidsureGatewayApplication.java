e8f9aec06657d2aa36d1ea1967fd1b17a53362fc
Athanasios <a.topaltsis@gmail.com>
2025-10-17 14:24:35 -0400

    added base project for api gateway

---
+++ bidsure-gateway/src/main/java/dev/naspo/bidsure_gateway/BidsureGatewayApplication.java
@@ -0,0 +1,13 @@
+package dev.naspo.bidsure_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidsureGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BidsureGatewayApplication.class, args);
+	}
+
+}
---
+++ bidsure-gateway/src/test/java/dev/naspo/bidsure_gateway/BidsureGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.naspo.bidsure_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BidsureGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
