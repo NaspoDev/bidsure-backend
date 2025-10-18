aa1cf593d91736f8b7304b728ded8dc4e551f1a3
Athanasios <a.topaltsis@gmail.com>
2025-10-17 20:59:14 -0400

    testing with user service

---
+++ bidsure-user-service/src/main/java/dev/naspo/bidsure_user_service/UserController.java
@@ -0,0 +1,14 @@
+package dev.naspo.bidsure_user_service;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping("")
+    private String[] getUsers() {
+        return new String[]{"Lauren", "Athy", "Justin", "Laya"};
+    }
+}
