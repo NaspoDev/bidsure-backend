d7447f92098a5c771f6784e5ddb67ea5b5d19680
Athanasios <a.topaltsis@gmail.com>
2025-10-17 20:02:31 -0400

    Added empty project for user-service

---
+++ bidsure-user-service/src/main/java/dev/naspo/bidsure_user_service/BidsureUserServiceApplication.java
@@ -0,0 +1,13 @@
+package dev.naspo.bidsure_user_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidsureUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BidsureUserServiceApplication.class, args);
+	}
+
+}
