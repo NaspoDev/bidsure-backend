f5ef5de09d90c95b7425bcf426a90dae64b2a5ff
Athanasios <a.topaltsis@gmail.com>
2025-10-23 14:23:31 -0400

    completed controller methods and hibernate integration

--- bidsure-user-service/src/main/java/dev/naspo/bidsure_user_service/UserController.java
+++ bidsure-user-service/src/main/java/dev/naspo/bidsure_user_service/UserController.java
@@ -1,38 +1,74 @@
 package dev.naspo.bidsure_user_service;
 
 import dev.naspo.bidsure_user_service.models.User;
+import jakarta.validation.Valid;
 import org.hibernate.Session;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
-    @GetMapping("/{id}")
-    public String getUser(@PathVariable int id) {
-        // TODO: implement proper logic
-        return "Will soon return the user with id: " + id;
-    }
-
     @PostMapping
-    public ResponseEntity<String> createUser(@RequestBody User user) throws ResponseStatusException {
+    public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             session.beginTransaction();
             session.persist(user);
             session.getTransaction().commit();
+            return ResponseEntity.status(HttpStatus.CREATED).body(user);
         } catch (Exception e) {
-            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Failed to create user.");
+            return ResponseEntity.internalServerError().build();
         }
-        return ResponseEntity.status(HttpStatus.CREATED).body("User created successfully.");
     }
 
-    // TODO: Make put and post mappings once user model is created
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUser(@PathVariable int id) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            session.beginTransaction();
+            User user = session.find(User.class, id);
+            session.getTransaction().commit();
+
+            if (user != null) {
+                return ResponseEntity.ok(user);
+            }
+            return ResponseEntity.notFound().build();
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable int id, @Valid @RequestBody User updatedUser) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            session.beginTransaction();
+            if (session.find(User.class, id) == null) {
+                return ResponseEntity.notFound().build();
+            }
+
+            updatedUser.setId(id);
+            session.merge(updatedUser);
+            session.getTransaction().commit();
+            return ResponseEntity.noContent().build();
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
+    }
 
     @DeleteMapping("/{id}")
-    public String deleteUser(@PathVariable int id) {
-        return "User with the id " + id + " has been deleted";
+    public ResponseEntity<String> deleteUser(@PathVariable int id) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            session.beginTransaction();
+            User user = session.find(User.class, id);
+            if (user == null) {
+                return ResponseEntity.notFound().build();
+            }
+            session.remove(user);
+            session.getTransaction().commit();
+            return ResponseEntity.ok().build();
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
     }
 }
--- bidsure-user-service/src/main/java/dev/naspo/bidsure_user_service/models/User.java
+++ bidsure-user-service/src/main/java/dev/naspo/bidsure_user_service/models/User.java
@@ -1,30 +1,43 @@
 package dev.naspo.bidsure_user_service.models;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "Users")
+@Getter
+@Setter
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
+    @NotEmpty
     private String username;
 
+    @NotEmpty
     private String password;
 
+    @Email
     private String email;
 
+    @NotEmpty
     @Column(name = "first_name")
     private String firstName;
 
+    @NotEmpty
     @Column(name = "last_name")
     private String lastName;
 
+    @NotEmpty
     @Column(name = "phone_number")
     private String phoneNumber;
 
+    @NotEmpty
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "address_id")
     private Address address;
