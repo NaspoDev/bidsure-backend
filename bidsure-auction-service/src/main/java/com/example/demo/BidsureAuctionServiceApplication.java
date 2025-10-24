ecfe2347393c8fc6be1fa6dc1c17a0e8c29b174f
Athanasios <a.topaltsis@gmail.com>
2025-10-24 18:05:23 -0400

    inital config auction service

---
+++ bidsure-auction-service/src/main/java/com/example/demo/BidsureAuctionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidsureAuctionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BidsureAuctionServiceApplication.class, args);
+	}
+
+}
