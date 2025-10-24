6a799419a2c0367b8db6e48ee4e50a9cea393b42
Athanasios <a.topaltsis@gmail.com>
2025-10-24 18:53:44 -0400

    payment service

---
+++ bidsure-payment-service/src/main/java/dev/naspo/bidsure_payment_service/BidsurePaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package dev.naspo.bidsure_payment_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidsurePaymentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BidsurePaymentServiceApplication.class, args);
+	}
+
+}
---
+++ bidsure-payment-service/src/test/java/dev/naspo/bidsure_payment_service/BidsurePaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.naspo.bidsure_payment_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BidsurePaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
