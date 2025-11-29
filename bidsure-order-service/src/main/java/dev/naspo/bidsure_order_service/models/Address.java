ad97a2da4aa8f1a342626c1b067140c690847349
Athanasios <a.topaltsis@gmail.com>
2025-11-29 14:36:58 -0500

    Removed some last remanence of manual hibernate implementation

--- bidsure-order-service/src/main/java/dev/naspo/bidsure_order_service/models/Address.java
+++ bidsure-order-service/src/main/java/dev/naspo/bidsure_order_service/models/Address.java
@@ -1,6 +1,5 @@
 package dev.naspo.bidsure_order_service.models;
 
-import jakarta.persistence.*;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -9,35 +8,26 @@ import lombok.Setter;
 
 @Getter
 @Setter
-@Entity
-@Table(name = "Addresses")
 public class Address {
-    @Id
-    @GeneratedValue
     private int id;
 
     @NotEmpty
     @Size(max = 50)
-    @Column(name = "first_name")
     private String firstName;
 
     @NotEmpty
     @Size(max = 50)
-    @Column(name = "last_name")
     private String lastName;
 
     @NotEmpty
-    @Column(name = "address_line_1")
     private String addressLine1;
 
-    @Column(name = "address_line_2")
     private String addressLine2;
 
     @NotEmpty
     private String city;
 
     @NotEmpty
-    @Column(name = "postal_code")
     @Size(min = 7, max = 7) // Ex. "L4J 5J2"
     private String postalCode;
 
@@ -46,10 +36,8 @@ public class Address {
     private String province;
 
     @NotEmpty
-    @Column(name = "phone_number")
     private String phoneNumber;
 
     @NotNull
-    @Column(name = "user_id")
     private Integer userId;
 }
--- bidsure-order-service/src/main/java/dev/naspo/bidsure_order_service/models/PaymentMethod.java
+++ bidsure-order-service/src/main/java/dev/naspo/bidsure_order_service/models/PaymentMethod.java
@@ -1,6 +1,5 @@
 package dev.naspo.bidsure_order_service.models;
 
-import jakarta.persistence.*;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
@@ -9,35 +8,27 @@ import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.validator.constraints.CreditCardNumber;
 
-@Entity
-@Table(name = "PaymentMethods")
 @Getter
 @Setter
 public class PaymentMethod {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @NotEmpty
     @Size(max = 50)
-    @Column(name = "first_name")
     private String firstName;
 
     @NotEmpty
     @Size(max = 50)
-    @Column(name = "last_name")
     private String lastName;
 
     @CreditCardNumber
-    @Column(name = "card_number")
     private String cardNumber;
 
     @Pattern(
             regexp = "^(0[1-9]|1[0-2])/\\d{2}$",
             message = "Expiry date must be in the format MM/YY and have a valid month (01–12)"
     )
-    @Column(name = "exp_date")
     private String expiryDate;
 
     @Pattern(
@@ -47,6 +38,5 @@ public class PaymentMethod {
     private String cvv;
 
     @NotNull
-    @Column(name = "user_id")
     private Integer userId;
 }
